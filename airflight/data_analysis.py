cb10a6df7e3890dc690e06c075cecea88b3efa11
ramilabd <ramsmart@mail.ru>
2021-09-12 21:54:57 +0500

    the entire module has been rewritten

--- airflight/data_analysis.py
+++ airflight/data_analysis.py
@@ -1,4 +1,4 @@
-import operator
+from datetime import datetime, timedelta
 from .parser_xml import get_xml_tree
 
 
@@ -29,27 +29,36 @@ def get_all_flights():
 
         all_flights.append(routs)
 
-    return all_flights
+    return list(map(add_total_travel_time, all_flights))
 
 
-def get_flights_sorted_price(compare=''):
-    all_flights = get_all_flights()
-    operation = {
-        'min': operator.lt,
-        'max': operator.gt,
-    }
+def get_flights_sorted_price(flights, reverse=False):
+    return sorted(flights, key=lambda flight: float(flight['Price']['TicketPrice']), reverse=reverse)
 
-    list_price = []
-    max_price = float(all_flights[0]['Price']['TicketPrice'])
 
-    for flight in all_flights:
-        price = float(flight['Price']['TicketPrice'])
-        index = all_flights.index(flight)
-        if operation[compare](price, max_price):
-            max_price = price
-            list_price = []
-            list_price.append(all_flights[index])
-        elif price == max_price:
-            list_price.append(all_flights[index])
+def get_flights_sorted_time(flights, reverse=False):
+    return sorted(flights, key=lambda flight: flight['TotalTravelTime'], reverse=reverse)
 
-    return list_price
+
+def get_flights_sorted_direction(source, destination):
+    all_flight = get_all_flights()
+    sorted_flights = []
+
+    for flight in all_flight:
+        if flight['flight_1']['Source'] == source and 'flight_2' in flight:
+            if flight['flight_2']['Destination'] == destination:
+                sorted_flights.append(flight)
+        elif flight['flight_1']['Destination'] == destination:
+                sorted_flights.append(flight)
+
+    return sorted_flights
+
+def add_total_travel_time(flight, str_time=False):
+    departure_time = flight['flight_1']['DepartureTimeStamp']
+    if 'flight_2' in flight:
+        arrival_time = flight['flight_2']['ArrivalTimeStamp']
+    else:
+        arrival_time = flight['flight_1']['ArrivalTimeStamp']
+    time_delta = datetime.strptime(arrival_time, '%Y-%m-%dT%H%M') - datetime.strptime(departure_time, '%Y-%m-%dT%H%M')
+    flight['TotalTravelTime'] = str(time_delta) if str_time else time_delta
+    return flight
