43877f7d9926da02bee59f002652b34b9269173e
ramilabd <ramsmart@mail.ru>
2021-09-13 23:20:01 +0500

    rewrite func "get_flights_sorted_direction"

--- airflight/data_analysis.py
+++ airflight/data_analysis.py
@@ -32,27 +32,6 @@ def get_all_flights():
     return list(map(add_total_travel_time, all_flights))
 
 
-def get_flights_sorted_price(flights, reverse=False):
-    return sorted(flights, key=lambda flight: float(flight['Price']['TicketPrice']), reverse=reverse)
-
-
-def get_flights_sorted_time(flights, reverse=False):
-    return sorted(flights, key=lambda flight: flight['TotalTravelTime'], reverse=reverse)
-
-
-def get_flights_sorted_direction(source, destination):
-    all_flight = get_all_flights()
-    sorted_flights = []
-
-    for flight in all_flight:
-        if flight['flight_1']['Source'] == source and 'flight_2' in flight:
-            if flight['flight_2']['Destination'] == destination:
-                sorted_flights.append(flight)
-        elif flight['flight_1']['Destination'] == destination:
-                sorted_flights.append(flight)
-
-    return sorted_flights
-
 def add_total_travel_time(flight, str_time=False):
     departure_time = flight['flight_1']['DepartureTimeStamp']
     if 'flight_2' in flight:
@@ -62,3 +41,26 @@ def add_total_travel_time(flight, str_time=False):
     time_delta = datetime.strptime(arrival_time, '%Y-%m-%dT%H%M') - datetime.strptime(departure_time, '%Y-%m-%dT%H%M')
     flight['TotalTravelTime'] = str(time_delta) if str_time else time_delta
     return flight
+
+
+def get_flights_sorted_price(flights, reverse=False, number_flights=None):
+    return sorted(flights, key=lambda flight: float(flight['Price']['TicketPrice']), reverse=reverse)[:number_flights]
+
+
+def get_flights_sorted_time(flights, reverse=False, number_flights=None):
+    return sorted(flights, key=lambda flight: flight['TotalTravelTime'], reverse=reverse)
+
+
+def get_flights_sorted_direction(source, destination):
+    all_flights = get_all_flights()
+    sorted_flights = []
+
+    for flight in all_flights:
+        if 'flight_2' not in flight:
+            if flight['flight_1']['Source'] == source and flight['flight_1']['Destination'] == destination:
+                sorted_flights.append(flight)
+        else:
+            if flight['flight_1']['Source'] == source and flight['flight_2']['Destination'] == destination:
+                sorted_flights.append(flight)
+
+    return sorted_flights
