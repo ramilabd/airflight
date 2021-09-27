a6f42b9cfbbf0fa0e99c0edc9b790ed74ea38e45
ramilabd <ramsmart@mail.ru>
2021-09-27 16:44:00 +0500

    added tests of the functions get_all_routes and get_optimal_route

--- tests/test_airflight.py
+++ tests/test_airflight.py
@@ -1,9 +1,11 @@
-# from ..airflight import __version__
-from airflight.data_analysis import get_all_flights, get_flights_sorted_price, get_flights_sorted_time, get_flights_sorted_direction
-import operator
-
-# def test_version():
-#     assert __version__ == '0.1.0'
+from airflight.data_analysis import (
+    get_all_flights,
+    get_flights_sorted_price,
+    get_flights_sorted_time,
+    get_flights_filtered_direction,
+    get_all_routes,
+    get_optimal_route
+)
 
 
 def test_get_all_flights():
@@ -11,66 +13,127 @@ def test_get_all_flights():
 
 
 def test_get_flights_sorted_price():
-    def checking_sorting_price(func, reverse=False):
-        sort_flights = func(get_all_flights(), reverse=reverse)
-        sort_correct = True
+    def is_correct_sorting_by_price(reverse=False):
+        flights = get_all_flights()
+        sort_flights_by_price = get_flights_sorted_price(flights, reverse=reverse)
+        is_correct_sort = True
 
         i, j = 0, 1
-        while sort_correct and j < len(sort_flights):
-            prev_price = float(sort_flights[i]['Price']['TicketPrice'])
-            next_price = float(sort_flights[j]['Price']['TicketPrice'])
+        while is_correct_sort and j < len(sort_flights_by_price):
+            prev_price = float(sort_flights_by_price[i]['Price']['TicketPrice'])
+            next_price = float(sort_flights_by_price[j]['Price']['TicketPrice'])
             if reverse:
                 if prev_price < next_price:
-                    sort_correct = False
+                    is_correct_sort = False
             elif prev_price > next_price:
-                    sort_correct = False
+                    is_correct_sort = False
             i += 1
             j += 1
 
-        return sort_correct
+        return is_correct_sort
 
-    assert checking_sorting_price(get_flights_sorted_price) == True
-    assert checking_sorting_price(get_flights_sorted_price, reverse=True) == True
-    assert checking_sorting_price(get_flights_sorted_price, reverse=True) == True
+    assert is_correct_sorting_by_price() == True
+    assert is_correct_sorting_by_price(reverse=True) == True
 
 
 def test_get_flights_sorted_time():
-    def checking_sorting_time(func, reverse=False):
-            sort_flights = func(get_all_flights(), reverse=reverse)
-            sort_correct = True
+    def is_correct_sorting_by_time(reverse=False):
+        flights = get_all_flights()
+        sort_flights_by_time = get_flights_sorted_time(flights, reverse=reverse)
+        is_correct_sort = True
 
-            i, j = 0, 1
-            while sort_correct and j < len(sort_flights):
-                prev_time = sort_flights[i]['TotalTravelTime']
-                next_time = sort_flights[j]['TotalTravelTime']
-                if reverse:
-                    if prev_time < next_time:
-                        sort_correct = False
-                elif prev_time > next_time:
-                        sort_correct = False
-                i += 1
-                j += 1
+        i, j = 0, 1
+        while is_correct_sort and j < len(sort_flights_by_time):
+            prev_time = sort_flights_by_time[i]['TotalTravelTime']
+            next_time = sort_flights_by_time[j]['TotalTravelTime']
+            if reverse:
+                if prev_time < next_time:
+                    is_correct_sort = False
+            elif prev_time > next_time:
+                    is_correct_sort = False
+            i += 1
+            j += 1
 
-            return sort_correct
+        return is_correct_sort
 
-    assert checking_sorting_time(get_flights_sorted_time) == True
-    assert checking_sorting_time(get_flights_sorted_time, reverse=True) == True
+    assert is_correct_sorting_by_time() == True
+    assert is_correct_sorting_by_time(reverse=True) == True
 
 
-def test_get_flights_sorted_direction():
-    def checking_sorted_direction(func, source, destination):
-        all_flights = get_all_flights()
-
-        sorted_flights_with_source_destination = func(source, destination)
+def test_get_flights_filtered_direction():
+    def is_correct_sorting_by_direction(source, destination):
+        flights = get_all_flights()
+        sorted_flights_with_source_destination = get_flights_filtered_direction(
+            flights, source, destination)
         sorted_flights_without_source_destination = []
 
-        for flight in all_flights:
+        for flight in flights:
             if 'flight_2' not in flight:
-                if source != flight['flight_1']['Source'] or destination != flight['flight_1']['Destination']:
+                if (source != flight['flight_1']['Source'] or
+                    destination != flight['flight_1']['Destination']):
                     sorted_flights_without_source_destination.append(flight)
-            elif source != flight['flight_1']['Source'] or destination != flight['flight_2']['Destination']:
+            elif (source != flight['flight_1']['Source'] or
+                    destination != flight['flight_2']['Destination']):
                     sorted_flights_without_source_destination.append(flight)
 
-        return len(sorted_flights_with_source_destination) + len(sorted_flights_without_source_destination) == len(all_flights)
+        return (len(sorted_flights_with_source_destination) +
+                len(sorted_flights_without_source_destination) == len(flights))
 
-    assert checking_sorted_direction(get_flights_sorted_direction, 'DXB', 'BKK') == True
+    assert is_correct_sorting_by_direction('DXB', 'BKK') == True
+
+
+def test_get_all_routes():
+    all_flights = get_all_flights()
+    total_count_flights = len(all_flights)
+    all_routes = get_all_routes(all_flights)
+    count_each_route = {}
+    for route in all_routes:
+        direction = '{0}-{1}-{2}'.format(route['Source'],
+                                        route.get('Transfer', None),
+                                        route['Destination'])
+        if direction in count_each_route:
+            count_each_route[direction] += 1
+        else:
+            count_each_route[direction] = 1
+
+    assert sum(count_each_route.values()) == total_count_flights
+
+
+def test_get_optimal_route():
+    all_flights = get_all_flights()
+    optimal_route = get_optimal_route(all_flights, 'DXB', 'BKK')
+
+    def get_median_time(flights):
+        times = []
+        for flight in flights:
+            times.append(flight['TotalTravelTime'])
+
+        sorted_times = sorted(times)
+        if len(sorted_times) % 2 == 0:
+            median = (sorted_times[(len(sorted_times) // 2) - 1] + sorted_times[len(sorted_times) // 2]) // 2
+        else:
+            median = sorted_times[len(sorted_times) // 2]
+
+        return median
+
+    def get_median_price(flights):
+        prices = []
+        for flight in flights:
+            prices.append(flight['Price']['TicketPrice'])
+
+        sorted_prices = sorted(list(map(float, prices)))
+        if len(sorted_prices) % 2 == 0:
+            median = (float(sorted_prices[(len(sorted_prices) // 2) - 1]) + float(sorted_prices[len(sorted_prices) // 2])) // 2
+        else:
+            median = float(sorted_prices[len(sorted_prices) // 2])
+
+        return median
+
+    median_time_all_flights = get_median_time(all_flights)
+    median_time_optimal_route = get_median_time(optimal_route)
+
+    median_price_all_flights = get_median_price(all_flights)
+    median_price_optimal_route = get_median_price(optimal_route)
+
+    assert median_time_optimal_route < median_time_all_flights
+    assert median_price_optimal_route < median_price_all_flights
