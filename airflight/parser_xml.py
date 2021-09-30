92b8a761a014482fd4051285e4f625caf829992a
ramilabd <ramsmart@mail.ru>
2021-09-30 20:49:51 +0500

    bugfix after linter checking

--- airflight/parser_xml.py
+++ airflight/parser_xml.py
@@ -1,30 +1,59 @@
+# -*- coding:utf-8 -*-
+"""Module parsing xml-file."""
+
+
 import hashlib
+from functools import lru_cache, partial
 
 from lxml import etree
 
-
-def sha256sum(filename):
-    h  = hashlib.sha256()
-    b  = bytearray(128*1024)
-    mv = memoryview(b)
-    with open(filename, 'rb', buffering=0) as f:
-        for n in iter(lambda : f.readinto(mv), 0):
-            h.update(mv[:n])
-        return h.hexdigest()
+FILE_PATH = './airflight/data/RS_Via-3.xml'
 
 
-def get_xml_tree(filepath):
-    memory_file_and_hash = {}
+def count_bytes(fileobject, mv):
+    """Return the read bytes of the object.
 
-    def memorize_file_and_hash():
-        file_hash = sha256sum(filepath)
+    Args:
+        fileobject (_io.FileIO): FileIO (return function "open()")
+        mv (bytearray): bytes-like object
 
-        if file_hash == memory_file_and_hash.get(file_hash):
-            return memory_file_and_hash.get('tree')
-        else:
-            tree = etree.parse(filepath)
-            memory_file_and_hash['file_hash'] = file_hash
-            memory_file_and_hash['tree'] = tree
-            return tree
+    Returns:
+        (int): count bytes of fileobject.
+    """
+    return fileobject.readinto(mv)
 
-    return memorize_file_and_hash
+
+def sha256sum(file_path):
+    """Return the calculated hash of the file.
+
+    Args:
+        file_path (str): path to xml file.
+
+    Returns:
+        hash (str): hash of file.
+    """
+    hash256 = hashlib.sha256()
+    mv = memoryview(bytearray(128 * 1024))
+
+    with open(file_path, 'rb', buffering=0) as fileobject:
+        partial_count_bytes = partial(count_bytes, fileobject, mv)
+        for num in iter(partial_count_bytes, 0):
+            hash256.update(mv[:num])
+        return hash256.hexdigest()
+
+
+@lru_cache
+def get_xml_tree(file_hash):
+    """Return tree of xml file (file object).
+
+    Memoizes file parsing xml data. If the file has not been modified,
+    it returns the file object from memory, otherwise it reads the
+    file, saves it and returns it.
+
+    Args:
+        file_hash (str): hash of the xml-file.
+
+    Returns:
+        ElementTree: tree of xml file (file object).
+    """
+    return etree.parse(FILE_PATH)
